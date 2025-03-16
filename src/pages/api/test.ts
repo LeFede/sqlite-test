84afd400d1b50f7cf4bbad6f3034a00e63b65e44
LeFede <lefede@gmail.com>
2025-03-16 16:48:09 -0300

    go

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import type { APIRoute } from "astro";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export const GET: APIRoute = async () => {
-  const entities = await prisma.entity.findMany({});
-  return new Response(JSON.stringify(entities), {
+  // const entities = await prisma.entity.findMany({});
+  return new Response(JSON.stringify("Hola!"), {
     status: 200,
   });
 };
