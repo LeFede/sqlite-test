09eb192718f1dd58f90a2c33d4357e9125d72aac
LeFede <lefede@gmail.com>
2025-03-16 16:46:40 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   output: "server",
 });
